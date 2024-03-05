f7a88097894d7a9456cd22951a82655961b909cb
MatheusAraujo121 <160777656+MatheusAraujo121@users.noreply.github.com>
2024-03-05 15:17:59 -0300

    Add files via upload

---
+++ Atividade 3/index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Segurança na web</title>
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="css/fontawesome/all.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body class="body">
+    <header class="top">
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+            <a class="navbar-brand zoom distance my-float" href="#"><h5>Home</h5></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link animated-button my-float" href="#oquee" style="color: white;"><h5>O que é segurança na web?</h5></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link animated-button my-float" href="#virus" style="color: white;"><h5>Vírus</h5></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link animated-button my-float" href="#csp" style="color: white;"><h5>Como se proteger</h5></a>
+                        </li>
+                        </ul>
+                    </ul>
+                </div>
+        </nav>
+    </header>
+    <div id="carouselExampleCaptions" class="carousel slide ms-auto" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="slide1.jpg" class="d-block w-100" alt="...">
+
+            </div>
+            <div class="carousel-item">
+                <img src="slide2.jpg" class="d-block w-100" alt="...">
+
+            </div>
+            <div class="carousel-item">
+                <img src="slide3.jpg" class="d-block w-100" alt="...">
+
+            </div>
+            <div class="carousel-item">
+                <img src="slide4.jpg" class="d-block w-100" alt="...">
+
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <br>
+    <br>
+    <br>
+        <h1 class=" titulo-bonito text-center"  id="oquee" > O que é segurança na web? </h1>
+    <br>
+    <div class="box">
+        <p>Segurança na web, também conhecida como cibersegurança, é um campo crucial na era digital, especialmente à medida que a sociedade se torna cada vez mais dependente da internet para realizar uma ampla gama de atividades, desde comunicações até transações financeiras e acesso a serviços essenciais. Em sua essência, a segurança na web refere-se às medidas e práticas destinadas a proteger sistemas, redes e dados contra ameaças cibernéticas, que podem incluir ataques maliciosos, roubo de dados, espionagem digital e interrupções de serviço.</p>
+        
+        <p>Com o aumento constante das atividades online, a segurança na web tornou-se uma preocupação central para indivíduos, empresas e governos em todo o mundo. Uma violação de segurança pode ter consequências devastadoras, tanto financeiramente quanto em termos de reputação. Portanto, é essencial implementar medidas eficazes para mitigar os riscos associados ao uso da internet.</p>
+        
+        <p>Existem várias camadas de segurança na web, desde o nível do usuário até o nível da infraestrutura de rede. No nível do usuário, práticas básicas de segurança, como o uso de senhas fortes, autenticação de dois fatores e a conscientização sobre phishing e outras táticas de engenharia social, são fundamentais. Além disso, manter software e sistemas atualizados com as últimas correções de segurança é essencial para evitar vulnerabilidades conhecidas.</p>
+        
+        <p>No nível da rede, firewalls, sistemas de detecção de intrusão e prevenção, bem como software antivírus, desempenham um papel crucial na defesa contra ameaças cibernéticas. Criptografia também é uma ferramenta poderosa para proteger a confidencialidade dos dados durante a transmissão pela internet.</p>
+        
+        <p>Além das medidas técnicas, a segurança na web também envolve aspectos legais e regulatórios. Muitos países têm leis e regulamentos específicos para proteger a privacidade dos usuários e exigir que as organizações protejam adequadamente os dados pessoais que coletam e armazenam.</p>
+        
+        <p>No entanto, apesar dos esforços contínuos para melhorar a segurança na web, os cibercriminosos estão constantemente desenvolvendo novas técnicas e táticas para contornar as defesas existentes. Portanto, a segurança na web é uma batalha em constante evolução que requer vigilância contínua e adaptação às ameaças emergentes.</p>
+    </div>
+
+    <br>
+    <br>
+
+    <h1 class=" titulo-bonito text-center"  id="virus" > O que são e quais são os tipos de virus? </h1>
+    <br>
+    <div class="box">
+        <p>Os vírus são programas de computador maliciosos projetados para se infiltrarem em sistemas de computador sem o consentimento do usuário e causarem danos, roubo de informações ou realizar outras atividades prejudiciais. Existem vários tipos de vírus, cada um com seus próprios métodos de propagação e efeitos. A lista a seguir conta com os tipos mais comuns de vírus:
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Vírus de Boot</h4>
+        <p>Este tipo de vírus infecta a área de inicialização de um dispositivo de armazenamento, como o disco rígido ou uma unidade flash USB. Quando o sistema é inicializado a partir do dispositivo infectado, o vírus é ativado e pode se espalhar para outros dispositivos conectados.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Vírus de Arquivo</h4>
+        <p> Esses vírus infectam arquivos executáveis, como programas ou scripts. Quando o arquivo infectado é executado, o vírus é ativado e pode se espalhar para outros arquivos executáveis no sistema.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Vírus de Macro</h4>
+        <p> Os vírus de macro são incorporados em documentos que suportam macros, como documentos do Microsoft Word ou planilhas do Excel. Quando o documento infectado é aberto e as macros são ativadas, o vírus é executado e pode se espalhar para outros documentos.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Vírus de Script</h4>
+        <p> Esses vírus são escritos em linguagens de script, como JavaScript ou VBScript, e são frequentemente transmitidos por e-mail ou sites maliciosos. Quando o script é executado, o vírus pode causar danos ao sistema ou se espalhar para outros computadores.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Vírus de Polimorfismo</h4>
+        <p> Esses vírus são capazes de modificar sua própria estrutura ou código para evitar a detecção por software antivírus. Eles podem se alterar cada vez que infectam um novo sistema, tornando-os mais difíceis de detectar e remover.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Vírus de Cavalo de Troia (Trojan)</h4>
+        <p> Os cavalos de Troia são programas que parecem ser benignos ou úteis, mas na realidade, eles contêm código malicioso que pode permitir a um invasor acessar e controlar o sistema infectado.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Worms (Vermes)</h4>
+        <p> Ao contrário dos vírus, os vermes são programas autônomos que se espalham automaticamente para outros computadores em uma rede, geralmente explorando vulnerabilidades de segurança. Eles podem se propagar rapidamente e causar danos significativos à rede.
+        <br>
+        <br>
+        <br>
+        <p>Estes são apenas alguns exemplos dos muitos tipos de vírus que existem. À medida que os métodos de ataque cibernético evoluem, novas variantes de vírus e técnicas maliciosas continuam a surgir, destacando a importância de manter sistemas atualizados e usar medidas de segurança proativas para proteger contra ameaças cibernéticas.
+    </div>
+    <br>
+    <br>
+    <br>
+        <h1 class=" titulo-bonito text-center"  id="csp" > Como se proteger </h1>
+    <br>
+    <div class="box">
+        <p>Proteger-se na web é fundamental para garantir a segurança de seus dados pessoais, financeiros e sensíveis. Estas são algumas medidas essenciais que você pode tomar para se proteger enquanto navega na internet:
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Use senhas fortes e únicas</h4>
+        <p>Crie senhas complexas e diferentes para cada conta online que você possui. Evite usar informações pessoais óbvias e opte por combinações de letras, números e caracteres especiais. Considere o uso de gerenciadores de senhas para ajudar a lembrar e gerenciar suas credenciais de forma segura.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Ative a autenticação de dois fatores (2FA)</h4>
+        <p>Além das senhas, habilite a autenticação de dois fatores sempre que possível. Isso adiciona uma camada extra de segurança, exigindo uma segunda forma de verificação, como um código enviado para o seu celular, para fazer login em suas contas.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Mantenha seu software atualizado</h4>
+        <p>Certifique-se de manter seu sistema operacional, navegadores da web, aplicativos e programas antivírus sempre atualizados com as últimas correções de segurança. As atualizações frequentemente corrigem vulnerabilidades conhecidas que podem ser exploradas por hackers.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Tenha cuidado ao clicar em links</h4>
+        <p>Evite clicar em links suspeitos ou desconhecidos em e-mails, mensagens instantâneas ou em sites não confiáveis. Verifique sempre a legitimidade dos links antes de clicar, especialmente se solicitarem informações pessoais ou financeiras.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Cuidado com phishing</h4>
+        <p>Esteja ciente de e-mails, mensagens ou telefonemas fraudulentos que tentam enganá-lo para revelar informações confidenciais, como senhas ou números de cartão de crédito. Nunca forneça informações pessoais ou financeiras em resposta a solicitações suspeitas.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Use uma conexão segura</h4>
+        <p>Ao acessar sites que exigem informações sensíveis, como dados bancários, certifique-se de que a conexão é segura. Procure pelo ícone de cadeado na barra de endereços do navegador e verifique se o URL começa com "https://" em vez de "http://".
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Faça backup regularmente</h4>
+        <p>Faça backups regulares de seus arquivos importantes e dados para evitar a perda de informações em caso de ransomware, falha do sistema ou outro tipo de ataque cibernético.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Utilize software antivírus e firewall</h4>
+        <p> Instale e mantenha atualizado um software antivírus confiável em seus dispositivos para protegê-los contra malware e outras ameaças. Além disso, ative e mantenha um firewall ativo para filtrar o tráfego de entrada e saída em sua rede.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Esteja ciente das configurações de privacidade</h4>
+        <p>Revise e ajuste regularmente as configurações de privacidade em suas contas de redes sociais, serviços online e dispositivos para controlar quem pode ver suas informações e como elas são compartilhadas.
+        <br>
+        <br>
+        <p><h4 class="text-center subtitulo">Eduque-se sobre segurança cibernética</h4>
+        <p>Mantenha-se atualizado sobre as últimas ameaças cibernéticas e melhores práticas de segurança, participando de cursos, seminários ou lendo recursos confiáveis ​​sobre o assunto.
+        <br>
+        <br>
+        <br>
+        <p>Seguindo essas práticas de segurança e permanecendo vigilante enquanto navega na web, você pode reduzir significativamente o risco de se tornar uma vítima de atividades maliciosas online.
+    </div>
+
+<footer> 
+    <div class="text-center p-3">
+      © 2024 Copyright: Matheus de A. Emidio
+    </div>
+</footer>
+</body>
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script> 
+    <script src="js/fontawesome/all.min.js"></script>
+</html>
