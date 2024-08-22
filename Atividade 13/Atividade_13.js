dedb781322765e1545e300d5b0db0f0707ba1516
MatheusAraujo121 <160777656+MatheusAraujo121@users.noreply.github.com>
2024-08-22 15:01:53 -0300

    Add files via upload

---
+++ Atividade 13/Atividade_13.js
@@ -0,0 +1,14 @@
+var aluno1 = {
+    RA: "0032876252005",
+    Nome: "Rafael"
+  };
+  
+var aluno2 = new Object();
+  aluno2.RA = "0032492622000";
+  aluno2.Nome = "Carlos";
+
+var aluno3 = {};
+  aluno3.RA = "0032433622010";
+  aluno3.Nome = "José";
+
+ 
