10644a222fa96d6ed5dc6811f359c1c76362b4b7
MatheusAraujo121 <160777656+MatheusAraujo121@users.noreply.github.com>
2024-09-17 21:01:47 -0300

    Delete Atividade 19/PWEBNode/Exercicios directory

--- Atividade 19/PWEBNode/Exercicios/QuartoS.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('file.txt');
-//a execução é criada até aqui até o arquivo ser lido
-console.log(data.toString());
--- Atividade 19/PWEBNode/Exercicios/callback2.js
+++
@@ -1,8 +0,0 @@
-function ExibeMensagensNaOrdem(mensagem, callback){
-    console.log(mensagem);
-    callback();
-}
-
-ExibeMensagensNaOrdem('Essa é a primeira mensagem exibida na ordem', function () {
-        console.log('Essa é a segunda mensagem exibida na ordem');
-})
